d104b83a5b4e6bab6a6c34b0b3dd4466a539b02b
StatQuestJourney Hub <mathewshem90@gmail.com>
2025-08-08 20:58:28 +0300

    Fix Firebase login auth

--- auth/firebase_auth.py
+++ auth/firebase_auth.py
@@ -1,9 +1,9 @@
+# auth/firebase_auth.py
 from auth.firebase_config import auth
 
 def login(email, password):
     try:
         user = auth.sign_in_with_email_and_password(email, password)
         return user
-    except Exception as e:
-        print("Login error:", e)
+    except:
         return None
